46244f16d801cd92ce297ac3df9d5522b24f93a5
zoepayadmin <intermediadoramwpay@gmail.com>
2026-02-11 02:37:19 -0300

    Refactor server to use Express and http-proxy-middleware

--- servidor.js
+++ servidor.js
@@ -1,27 +1,17 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const proxy = httpProxy.createProxyServer({
+const app = express();
+
+app.use('/', createProxyMiddleware({
   target: 'https://venomcheckoutpremium.lovable.app',
   changeOrigin: true,
-  secure: true,
   headers: {
     host: 'venomcheckoutpremium.lovable.app'
   }
-});
-
-proxy.on('error', (err, req, res) => {
-  console.error('Proxy error:', err.message);
-  res.writeHead(502);
-  res.end('Proxy error');
-});
-
-const server = http.createServer((req, res) => {
-  console.log(`${req.method} ${req.headers.host}${req.url}`);
-  proxy.web(req, res);
-});
+}));
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Proxy running on port ${PORT}`);
 });
